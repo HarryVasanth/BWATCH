e9c2a7e5c3281ddd42f38810469a648c99a4c7a4
Harry <mailsuthan@gmail.com>
2017-11-12 11:15:59 +0000

    Final local commit
    Last local commit before publishing this branch publicly to the GitHub

---
+++ mobile/src/androidTest/java/com/harry/bwatch/ApplicationTest.java
@@ -0,0 +1,19 @@
+/*
+ * Written by Harry Vasanth (harry.vasanth@m-iti.org)
+ * Copyright (c) 2017.
+ * Please acknowledge the creator by giving him/her credit for the work
+ */
+
+package com.harry.bwatch;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
